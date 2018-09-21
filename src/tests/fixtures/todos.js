1590b3c447ce74d0c224a31b5bd9d433aa08c8d3
Harshal <harshal.limaye.in@gmail.com>
2018-09-21 10:07:32 +0530

    testing todos reducer

---
+++ src/tests/fixtures/todos.js
@@ -0,0 +1,24 @@
+const todos = [
+    {
+        id: 3,
+        text: 'Write test cases',
+        completed: true
+    },
+    {
+        id: 2,
+        text: 'create container components',
+        completed: false
+    },
+    {
+        id: 1,
+        text: 'fix bugs',
+        completed: false
+    },
+    {
+        id: 0,
+        text: 'release next version',
+        completed: false
+    }
+]
+
+export default todos;
---
+++ src/tests/reducers/todos.test.js
@@ -0,0 +1,38 @@
+import todosReducer from '../../reducers/todos';
+import TODOLIST from '../fixtures/todos';
+
+let todos;
+
+const applyReducer = (state, action) => todosReducer(state, action); 
+
+describe('testing todos reducer', () => {
+    beforeEach(() => {
+        todos = TODOLIST;
+    });
+
+    test('should return previous state if action is unknown', () => {
+        const updatedTodos = applyReducer(todos, { type: 'SUBSTRACT', text: 'new todo' });
+        expect(updatedTodos).toEqual(todos);
+    })
+
+    test('should create a new todo item', () => {
+        const updatedTodos = applyReducer(todos, { type: 'ADD', text: 'new todo' });
+        expect(updatedTodos.length).toBe(5);
+        expect(updatedTodos[0]).toEqual({ id: 4, text: 'new todo', completed: false });
+    });
+
+    test('should mark a todo item as completed', () => {
+        const updatedTodos = applyReducer(todos, { type: 'COMPLETE', id: 0 });
+        expect(updatedTodos[0].completed).toBe(true);
+    });
+
+    test('should mark a todo item as incomplete', () => {
+        const updatedTodos = applyReducer(todos, { type: 'COMPLETE', id: 3 });
+        expect(updatedTodos[3].completed).toBe(false);
+    });
+
+    test('should delete a todo item', () => {
+        const updatedTodos = applyReducer(todos, { type: 'DELETE', id: 3 });
+        expect(updatedTodos.length).toBe(3);
+    });
+});
