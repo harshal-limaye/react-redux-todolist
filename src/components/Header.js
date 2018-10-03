aefcd64700c42ba46239d17543e7546fd13c3be4
Harshal <harshal.limaye.in@gmail.com>
2018-10-03 17:00:31 +0530

    adding default prop to header for apptitle

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,15 @@ class Header extends React.Component {
     render() {
         return (
             <div className="todo-header">
-                <h1>toDoList</h1>
+                <h1>{ this.props.appTitle }</h1>
                 <FilterOptions />
             </div>
         );
     }
+}
+
+Header.defaultProps = {
+    appTitle: 'toDoList'
 }
 
 export default Header;
